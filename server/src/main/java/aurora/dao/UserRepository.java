0b5f899bf748ebb6a979e0e165e8f726c9ba6ee5
prophet12505 <1250523689@qq.com>
2023-02-04 16:46:38 -0600

    initial commit

--- server/src/main/java/aurora/dao/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package aurora.dao;
-
-
-import aurora.entity.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<User, Long> {
-
-}
